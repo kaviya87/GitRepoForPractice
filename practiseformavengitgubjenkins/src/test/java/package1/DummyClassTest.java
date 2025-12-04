1fd4be0c9c9020659f0d8036c938e4b2ad83d4b2
brahm <brahm@192.168.1.24>
2025-12-04 08:20:57 +0530

    first commit

---
+++ practiseformavengitgubjenkins/src/test/java/package1/DummyClassTest.java
@@ -0,0 +1,19 @@
+package package1;
+
+
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+
+public class DummyClassTest {
+	
+      @Test
+      public void demo1() {
+	  Reporter.log("hi github",true);
+
+	}
+	
+	
+
+}
